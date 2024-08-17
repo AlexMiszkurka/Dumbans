3ead5dcd70d58cf96e0fe7145ff32927a56efbf3
Alex <alexmis8@gmail.com>
2024-08-17 16:21:08 -0700

    Adding downloads tab

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
         <TabList>
           <Tab>Videos</Tab>
           <Tab>Chat</Tab>
+          <Tab>Downloads</Tab>
         </TabList>
         <TabPanel>
           <h2>Leon's Videos</h2>
@@ -26,6 +27,33 @@ function App() {
           <h2>Chat Box</h2>
           <body>
             Chat box will be here
+          </body>
+        </TabPanel>
+        <TabPanel>
+          <h2>Downloads</h2>
+          <body>
+          <Tabs>
+              <TabList>
+                <Tab>Viruses</Tab>
+                <Tab>Artwork</Tab>
+                <Tab>Server Stuff</Tab>
+              </TabList>
+              <TabPanel>
+                <body>
+                  Viruses coming soon
+                </body>
+              </TabPanel>
+              <TabPanel>
+                <body>
+                  Artwork coming soon
+                </body>
+              </TabPanel>
+              <TabPanel>
+                <body>
+                  Server stuff coming soon
+                </body>
+              </TabPanel>
+            </Tabs>
           </body>
         </TabPanel>
       </Tabs>
