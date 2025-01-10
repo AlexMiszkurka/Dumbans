c99722530dbb6e4b7bd57f654d036c3e9e08107b
Leon Miszkurka <leonmiszkurka@gmail.com>
2025-01-10 11:01:10 -0800

    Adding QR-codes@1.0.0

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import qrCode from './Images/LM-Gmail.png'; // Corrected the path
 
-console.log("loading.. website");
+console.log("loading... website");
 
 window.alert("welcome to dumbans");
-
 
 function App() {
   return (
@@ -18,9 +18,10 @@ function App() {
       <Tabs>
         <TabList>
           <Tab>Videos</Tab>
-          <Tab>Chat</Tab>
+          <Tab>Random stuff</Tab>
           <Tab>Downloads</Tab>
           <Tab>Store</Tab>
+          <Tab>Account</Tab>
         </TabList>
         <TabPanel>
           <h2>Videos</h2>
@@ -29,33 +30,16 @@ function App() {
           </body>
         </TabPanel>
         <TabPanel>
-          <h2>Chat Box</h2>
+          <h2>Random stuff</h2>
+            Random stuff TBD
           <body>
-            <form>
-              <div>
-              <label for="ET1">Enter text:</label>
-              <input type="text" id="ET1" text="ET1" placeholder="TEXT"></input>
-              </div>
-              <input type="submit"></input>
-
-              <br>
-              </br>
-              <br>
-              </br>
-
-
-              <div>
-              <label for="EC1">Enter text:</label>
-              <input type="text" id="EC1" text="EC1" placeholder="COMMENT"></input>
-              </div>
-              <input type="submit"></input>
-            </form>
+          <img src={qrCode} alt="QR Code Email" style={{ width: '100px', height: '100px', border: '1px solid red' }} />
           </body>
         </TabPanel>
         <TabPanel>
           <h2>Downloads</h2>
           <body>
-          <Tabs>
+            <Tabs>
               <TabList>
                 <Tab>Programs</Tab>
                 <Tab>Artwork</Tab>
@@ -63,7 +47,7 @@ function App() {
               </TabList>
               <TabPanel>
                 <body>
-                Programs coming soon
+                  Programs coming soon
                 </body>
               </TabPanel>
               <TabPanel>
@@ -79,8 +63,33 @@ function App() {
             </Tabs>
           </body>
         </TabPanel>
+        <TabPanel>
+          <h2>Store</h2>
+          <body>
+            Store items TBD
+          </body>
+        </TabPanel>
+        <TabPanel>
+          <h2>Account</h2>
+          <body>
+            <form>
+              <div>
+                <label htmlFor="username">Username:</label>
+                <input type="text" id="username" placeholder="Username" />
+              </div>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" placeholder="Email" />
+              </div>
+              <div>
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" placeholder="Password" />
+              </div>
+              <input type="submit" value="Sign Up" />
+            </form>
+          </body>
+        </TabPanel>
       </Tabs>
-     <script src="pop-ups.js"></script>
     </div>
   );
 }
